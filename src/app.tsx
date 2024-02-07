454293d5624cca0ff8efdea9e29aff1b054acf1c
Felipe Ribeiro <piporibeiro@gmail.com>
2024-02-07 17:35:58 -0300

    feat: add NewNoteCard component and update NoteCard component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import nlwLogo from './assets/logo-nlw-experts.svg'
+import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
 export function App() {
@@ -15,28 +16,13 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
-        <div className="space-y-3 rounded-md bg-slate-700 p-5">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamente.
-          </p>
-        </div>
+        <NewNoteCard />
         <NoteCard
-          date="2"
-          content=" Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
-            impedit corrupti recusandae! Necessitatibus fugiat recusandae, est
-            autem qui minus nisi, rerum consectetur placeat nobis iure sed in
-            dignissimos, sint nihil."
-        />
-        <NoteCard
-          date="4"
-          content=" Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
-            impedit corrupti recusandae! Necessitatibus fugiat recusandae, est
-            autem qui minus nisi, rerum consectetur placeat nobis iure sed in
-            dignissimos, sint nihil."
+          note={{
+            date: new Date().toISOString(),
+            content:
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi impedit corrupti recusandae! Necessitatibus fugiat recusandae, est autem qui minus nisi, rerum consectetur placeat nobis iure sed in dignissimos, sint nihil.',
+          }}
         />
       </div>
     </div>
